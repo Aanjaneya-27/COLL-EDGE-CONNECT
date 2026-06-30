225caa5fe74bbb22452f5c21d18619eb10cfe62d
Aanjaneya Dikhit <dikhitanjaneyat03@gmail.com>
2026-06-30 10:51:10 +0530

    fourth commit

--- Frontend/src/services/TaskService.js
+++ Frontend/src/services/TaskService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/tasks"
+    baseURL: "https://coll-edge-connect-2.onrender.com/api/tasks"
 });
 
 export const getTask = () => API.get("/");
